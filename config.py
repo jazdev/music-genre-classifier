72a960096cbb6966af29581862f22d5e340b02b9
Jasdev Singh <jasdev@live.in>
2014-05-03 13:53:10 -0700

    auto gen model dir

--- config.py
+++ config.py
@@ -31,7 +31,9 @@ DATA_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
 
 CHART_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "charts")
 
-for d in [DATA_DIR, CHART_DIR]:
+MODEL_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "saved_model_fft")
+
+for d in [DATA_DIR, CHART_DIR, MODEL_DIR]:
     if not os.path.exists(d):
         os.mkdir(d)
 
